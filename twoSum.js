0e768398ad525978eccf3a220463452c57ea9585
patic457 <112005137+patic457@users.noreply.github.com>
2023-02-24 21:03:29 +0700

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -7,11 +7,15 @@
 
 
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-  for (let j = i + 1; j < nums.length; j++) {
-    if (nums[i] + nums[j] === target) {
-      return [i, j];
+    let indexA,indexB;
+
+    for (indexA = 0; indexA < nums.length; indexA++) {
+  for (indexB = indexA + 1; indexB < nums.length; indexB++) {
+    if (nums[indexA] + nums[indexB] === target) {
+      return [indexA, indexB];
     }
   }
 }
 };
+
+
