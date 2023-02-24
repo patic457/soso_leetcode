6955c82b6d19164f727688a5f6f2a7664b2425cb
patic457 <112005137+patic457@users.noreply.github.com>
2023-02-24 22:56:39 +0700

    4. Median of Two Sorted Arrays
    4. Median of Two Sorted Arrays

---
+++ aa.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+
+var mergeAndSort = function(nums1, nums2){
+
+    //Merge Array
+    numAll = [].concat(nums1, nums2);
+
+    //Sort Array
+    numAll.sort();
+}
+
+var findOddAndEven =  function(numMergeAndSort){
+
+    //Count
+    countNumAll = numMergeAndSort.length;
+
+    //Odd or Even
+    if(countNumAll%2===0){
+      
+    }else{}
+
+    //if()
+
+
+
+
+}
+
+var findMedianSortedArrays = function(nums1, nums2) {
+
+    //Merge and Sort Function
+    numMergeAndSort = mergeAndSort(nums1, nums2);
+
+    a = findOddAndEven(numMergeAndSort);
+
+ 
+
+    console.log(numAll);
+    console.log(countNumAll);
+
+
+    //Sort Array
+    countNums1 = nums1.length;
+};
+
+a = [1,2];
+b = [4,3,5];
+
+findMedianSortedArrays(a,b);
